466c7a0896e7883e03787cd1477aa116e037d05c
AbitBrookish <brooksjohnson25@gmail.com>
2023-05-01 15:25:53 -0400

    initialize class, create var, and add listener

---
+++ scripts/item_form.js
@@ -0,0 +1,5 @@
+const productsController = new ProductsController;
+
+// create eventListener for the submit event
+const submit = document.querySelector('#form-submit');
+submit.addEventListener();
