d6e7132ffbfb9c66b656c8b5f5e8de17f2e832c1
AbitBrookish <brooksjohnson25@gmail.com>
2023-05-01 12:47:40 -0400

    modify constructor and test instances

--- scripts/productsController.js
+++ scripts/productsController.js
@@ -1,17 +1,31 @@
 // create class 
 class ProductsController {
-  constructor() {
+  constructor(currentId) {
     this.product = [];
-    this.currentId = 0;
+    currentId = 0;
+    this.id = currentId;
   }
 
   addProduct(category, name, description, manufacturer) {
-    this.currentId++;
-    this.product.push(category, name, description, manufacturer);
+    this.id++;
+    const item = {category, name, description, manufacturer};
+    this.product.push(item);
   }
 }
 
-// // test class
-// const Shoes = new ProductsController;
-// Shoes.addProduct('womens apparel', 'ultra-bounce runner', 'lightweight shoes meant for speedwalking and jogging', 'Nike');
-// console.log(Shoes);
+// test class
+const Shoes = new ProductsController;
+Shoes.addProduct('womens apparel', 'ultra-bounce runner', 'lightweight shoes meant for speedwalking and jogging', 'Nike');
+console.log(Shoes);
+
+const Pants = new ProductsController;
+Pants.addProduct('mens apparel', 'flex runner', 'Thin fabric', 'Fabletics Men');
+console.log(Pants);
+
+const Watch = new ProductsController;
+Watch.addProduct('accessories', 'apple watch', 'apple watch', 'Apple');
+console.log(Watch);
+
+const Bra = new ProductsController;
+Bra.addProduct('womens apparel', 'The Holder', 'They hold them in place so you don\'t have to', 'Shien');
+console.log(Bra);
